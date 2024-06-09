d2d7febd356ee7de1c50fc377486023229135a05
Pierre LEJEUNE <darkanakin41@gmail.com>
2024-06-09 14:19:44 +0200

    chore: remove files

--- storybook-static/assets/Keystroke.stories-D2o2Uiu2.js
+++
@@ -1,5 +0,0 @@
-import{f as s}from"./index-Cq_GZlP2.js";import{K as a}from"./Keystroke-cC3_-6Gz.js";import"./index-Bqoxw6Vv.js";import"./jsx-runtime-BZf_YgVj.js";import"./index-CEThVCg_.js";const y={title:"Atoms/Keystroke",component:a,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{keystone:{control:"text"}},args:{onPress:s()}},r={args:{keystone:"F"}};var e,o,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    keystone: 'F'
-  }
-}`,...(t=(o=r.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const d=["Primary"];export{r as Primary,d as __namedExportsOrder,y as default};
